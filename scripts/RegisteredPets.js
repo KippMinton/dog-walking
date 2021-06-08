40636b5903a89e8401019e23b4659e343c289e7a
KippMinton <luciankippminton@gmail.com>
2021-06-07 23:14:07 -0500

    added click event to pets column that shows walker

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,31 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers} from "./database.js"
 
+
+document.addEventListener(
+    "click", 
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [, petId] = itemClicked.id.split("--")
+        
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+                    let walker = walkers.find(item => item.id === pet.walkerId)
+                    window.alert(`${pet.name} is being walked by ${walker.name}`)
+                }
+            }
+        }
+    }    
+)        
+
+const walkers = getWalkers()
 const pets = getPets()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
